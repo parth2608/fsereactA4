5e07af5e5eda01225d69ef86aa90644842353e0a
Parth Shah <parth.shah2118@gmail.com>
2022-03-31 15:32:18 -0400

    first commit

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -5,7 +5,7 @@ import {useEffect, useState} from "react";
 const MyLikes = () => {
     const [likedTuits, setLikedTuis] = useState([]);
     const findTuitsILike = () =>
-        service.findAllTuitsLikedByUser("my")
+        service.findAllTuitsLikedByUser("me")
             .then((tuits) => setLikedTuis(tuits));
     useEffect(findTuitsILike, []);
     
