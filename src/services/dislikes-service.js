1c9108e1aa6e0bec0765a9dddcf8fa0afa22882b
Parth Shah <parth.shah2118@gmail.com>
2022-03-31 21:38:30 -0400

    first commit

--- src/services/dislikes-service.js
+++ src/services/dislikes-service.js
@@ -19,3 +19,7 @@ export const findAllUsersThatDislikedTuit = (tid) =>
 export const userDislikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then((response) => response.data);
+
+export const userTogglesTuitDislikes = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+        .then(response => response.data);
