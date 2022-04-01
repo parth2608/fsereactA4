ac04b57ea05c8569ff0dbcb46bdd2f5a9701075f
Parth Shah <parth.shah2118@gmail.com>
2022-03-31 22:39:34 -0400

    first commit

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
+import * as service from "../../services/likes-service";
+import * as dis_service from "../../services/dislikes-service";
 
 const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit = () => {}}) => {
     return (
@@ -14,11 +16,11 @@ const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit = () => {}}) => {
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
-                tuit.stats && tuit.stats.likes > 0 &&
+                tuit.stats && tuit.stats.likedByMe ===true &&
                   <i className="fa-solid fa-thumbs-up"></i>
               }
               {
-                tuit.stats  && tuit.stats.likes <= 0 &&
+                tuit.stats && tuit.stats.likedByMe === false &&
                   <i className="fa-regular fa-thumbs-up"></i>
               }
             {tuit.stats && tuit.stats.likes}
@@ -27,11 +29,11 @@ const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit = () => {}}) => {
           <div className="col">
               <span onClick={() => dislikeTuit(tuit)}>
                   {
-                      tuit.stats &&  tuit.stats.dislikes > 0 &&
+                      tuit.stats &&  tuit.stats.dislikedByMe === true &&
                       <i className="fa-solid fa-thumbs-down"></i>
                   }
                   {
-                      tuit.stats && tuit.stats.dislikes <= 0 &&
+                      tuit.stats && tuit.stats.dislikedByMe === false &&
                       <i className="fa-regular fa-thumbs-down"></i>
                   }
                   {tuit.stats && tuit.stats.dislikes}
