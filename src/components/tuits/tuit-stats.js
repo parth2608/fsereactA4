68c7f2c149249789c64f04c512e2055e375fc510
Parth Shah <parth.shah2118@gmail.com>
2022-04-02 13:15:47 -0400

    Update tuit-stats.js

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useEffect} from "react";
+import * as service from "../../services/likes-service";
+import * as dis_service from "../../services/dislikes-service";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit = () => {}}) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -14,16 +16,29 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
-                  tuit.stats && tuit.stats.likes > 0 &&
-                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                tuit.stats && tuit.stats.likedByMe ===true &&
+                  <i className="fa-solid fa-thumbs-up"></i>
               }
               {
-                  tuit.stats && tuit.stats.likes <= 0 &&
-                  <i className="far fa-heart me-1"></i>
+                tuit.stats && tuit.stats.likedByMe === false &&
+                  <i className="fa-regular fa-thumbs-up"></i>
               }
             {tuit.stats && tuit.stats.likes}
           </span>
         </div>
+          <div className="col">
+              <span onClick={() => dislikeTuit(tuit)}>
+                  {
+                      tuit.stats &&  tuit.stats.dislikedByMe === true &&
+                      <i className="fa-solid fa-thumbs-down"></i>
+                  }
+                  {
+                      tuit.stats && tuit.stats.dislikedByMe === false &&
+                      <i className="fa-regular fa-thumbs-down"></i>
+                  }
+                  {tuit.stats && tuit.stats.dislikes}
+              </span>
+          </div>
         <div className="col">
           <i className="far fa-inbox-out"></i>
         </div>
